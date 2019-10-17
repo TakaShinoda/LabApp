a570086b4497feedb8fe545e30a4da26475d7b4d
Takahiro Shinoda <45593212+TakaShinoda@users.noreply.github.com>
2019-10-17 19:31:23 +0900

    Update store.js

--- store.js
+++ store.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 
 // Firebaseの初期化
 const firebaseConfig = {
-    apiKey: "AIzaSyCEJofsTR_yavt5svTPuMbzhlQgH8EjpyQ",
-    authDomain: "labapp-2cb54.firebaseapp.com",
-    databaseURL: "https://labapp-2cb54.firebaseio.com",
-    projectId: "labapp-2cb54",
-    storageBucket: "labapp-2cb54.appspot.com",
-    messagingSenderId: "943558314925",
-    appId: "1:943558314925:web:3885713334b75bcd8c2f24",
-    measurementId: "G-ZB9XJT09RX"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 var fireapp;
 try {
