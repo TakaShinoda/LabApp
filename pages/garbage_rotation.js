e4312d13b80461ec58746e99bce7df424a00a072
Takahiro <tkhr_621@docomo.ne.jp>
2019-10-24 23:56:22 +0900

    Update pages

--- pages/garbage_rotation.js
+++ pages/garbage_rotation.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Member from '../components/Member';
 
 export default () => (
     <Layout header='Whiteboard' title='ゴミローテーション'>
         <p>工事中...</p>
+        <Member />
         <Link href='/'>
             <a>&lt;&lt; 戻る</a>
         </Link>
