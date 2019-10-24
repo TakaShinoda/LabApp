fa84a27655f5e159e7ef5fe22adca4fbcf511843
Takahiro <tkhr_621@docomo.ne.jp>
2019-10-24 23:57:48 +0900

    add Member.jsx

---
+++ components/Member.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Member extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>山田 一郎</li>
+                    <li>山田 二郎</li>
+                    <li>山田 三郎</li>
+                    <li>山田 四郎</li>
+                    <li>山田 五郎</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Member;
