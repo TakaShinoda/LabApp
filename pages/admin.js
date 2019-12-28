eb5284aae5a97a66fba9e9e383f38370890b35ea
Takahiro <tkhr_621@docomo.ne.jp>
2019-12-28 22:25:26 +0900

    管理者画面の修正

--- pages/admin.js
+++ pages/admin.js
@@ -1,3 +1,15 @@
+import Layout from '../components/Layout';
+import Admin from '../components/Admin';
+import Link from 'next/link';
+
 export default () => (
-    <h1>管理者画面</h1>
+    <Layout header='Whiteboard' title='Admin'>
+        <p>管理者画面</p>
+        <Admin />
+        <Link href='/'>
+            <a>&lt;&lt; 戻る</a>
+        </Link>
+    </Layout>
+    
+    
 );
