033cacb0ee2a0b52034fdefea4038bbf6c5db28f
Takahiro <tkhr_621@docomo.ne.jp>
2019-12-28 22:38:23 +0900

    Add Admin.jsx

---
+++ components/Admin.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <ui>
+                    <li>ログイン後ゴミ捨てローテーションを管理できるようにする</li>
+                    <li>現在準備中です</li>
+                </ui>
+            </div>
+        )
+    }
+}
+
+export default Admin;
